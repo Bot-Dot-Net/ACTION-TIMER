e6916fd3ad65d408a94bfdcb09b0bb4e5e8cf9f4
BOT444 <mouryarajesh35@gmail.com>
2026-02-22 21:20:17 +0530

    initial

---
+++ Form1.Designer.cs
@@ -0,0 +1,306 @@
+﻿namespace ACTION_TIMER
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            panel1 = new Panel();
+            dtpClockTime = new DateTimePicker();
+            rdoClock = new RadioButton();
+            rdoCountdown = new RadioButton();
+            label4 = new Label();
+            lblCountdown = new Label();
+            txtTime = new TextBox();
+            lblTime = new Label();
+            panel4 = new Panel();
+            label3 = new Label();
+            label2 = new Label();
+            btnCancel = new Button();
+            btnStart = new Button();
+            panel2 = new Panel();
+            txtProgramName = new TextBox();
+            cmbAction = new ComboBox();
+            panel3 = new Panel();
+            label1 = new Label();
+            timer1 = new System.Windows.Forms.Timer(components);
+            panel1.SuspendLayout();
+            panel4.SuspendLayout();
+            panel2.SuspendLayout();
+            panel3.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BorderStyle = BorderStyle.FixedSingle;
+            panel1.Controls.Add(dtpClockTime);
+            panel1.Controls.Add(rdoClock);
+            panel1.Controls.Add(rdoCountdown);
+            panel1.Controls.Add(label4);
+            panel1.Controls.Add(lblCountdown);
+            panel1.Controls.Add(txtTime);
+            panel1.Controls.Add(lblTime);
+            panel1.Controls.Add(panel4);
+            panel1.Dock = DockStyle.Left;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(240, 225);
+            panel1.TabIndex = 0;
+            // 
+            // dtpClockTime
+            // 
+            dtpClockTime.Format = DateTimePickerFormat.Time;
+            dtpClockTime.Location = new Point(13, 138);
+            dtpClockTime.Name = "dtpClockTime";
+            dtpClockTime.ShowUpDown = true;
+            dtpClockTime.Size = new Size(127, 23);
+            dtpClockTime.TabIndex = 11;
+            dtpClockTime.ValueChanged += dtpClockTime_ValueChanged;
+            // 
+            // rdoClock
+            // 
+            rdoClock.AutoSize = true;
+            rdoClock.Location = new Point(187, 145);
+            rdoClock.Name = "rdoClock";
+            rdoClock.Size = new Size(14, 13);
+            rdoClock.TabIndex = 10;
+            rdoClock.TabStop = true;
+            rdoClock.UseVisualStyleBackColor = true;
+            rdoClock.CheckedChanged += rdoClock_CheckedChanged;
+            // 
+            // rdoCountdown
+            // 
+            rdoCountdown.AutoSize = true;
+            rdoCountdown.Location = new Point(187, 74);
+            rdoCountdown.Name = "rdoCountdown";
+            rdoCountdown.Size = new Size(14, 13);
+            rdoCountdown.TabIndex = 0;
+            rdoCountdown.TabStop = true;
+            rdoCountdown.UseVisualStyleBackColor = true;
+            rdoCountdown.CheckedChanged += rdoCountdown_CheckedChanged;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.ForeColor = Color.Aquamarine;
+            label4.Location = new Point(13, 120);
+            label4.Name = "label4";
+            label4.Size = new Size(115, 15);
+            label4.TabIndex = 9;
+            label4.Text = "Enter time (in Clock)";
+            // 
+            // lblCountdown
+            // 
+            lblCountdown.AutoSize = true;
+            lblCountdown.BackColor = Color.Transparent;
+            lblCountdown.ForeColor = Color.Aquamarine;
+            lblCountdown.Location = new Point(13, 189);
+            lblCountdown.Name = "lblCountdown";
+            lblCountdown.Size = new Size(81, 15);
+            lblCountdown.TabIndex = 8;
+            lblCountdown.Text = "Time left  :  —";
+            // 
+            // txtTime
+            // 
+            txtTime.Location = new Point(13, 70);
+            txtTime.Name = "txtTime";
+            txtTime.PlaceholderText = "(empty)";
+            txtTime.Size = new Size(127, 23);
+            txtTime.TabIndex = 3;
+            // 
+            // lblTime
+            // 
+            lblTime.AutoSize = true;
+            lblTime.ForeColor = Color.Aquamarine;
+            lblTime.Location = new Point(12, 52);
+            lblTime.Name = "lblTime";
+            lblTime.Size = new Size(128, 15);
+            lblTime.TabIndex = 2;
+            lblTime.Text = "Enter time (in minutes)";
+            // 
+            // panel4
+            // 
+            panel4.BackColor = Color.FromArgb(26, 26, 26);
+            panel4.Controls.Add(label3);
+            panel4.Controls.Add(label2);
+            panel4.Dock = DockStyle.Top;
+            panel4.Location = new Point(0, 0);
+            panel4.Name = "panel4";
+            panel4.Size = new Size(238, 29);
+            panel4.TabIndex = 1;
+            // 
+            // label3
+            // 
+            label3.Anchor = AnchorStyles.Top | AnchorStyles.Bottom;
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.ForeColor = Color.Aquamarine;
+            label3.Location = new Point(80, 5);
+            label3.Name = "label3";
+            label3.Size = new Size(58, 21);
+            label3.TabIndex = 1;
+            label3.Text = "TIMER";
+            // 
+            // label2
+            // 
+            label2.Anchor = AnchorStyles.Top | AnchorStyles.Bottom;
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.ForeColor = Color.Aquamarine;
+            label2.Location = new Point(257, 0);
+            label2.Name = "label2";
+            label2.Size = new Size(79, 21);
+            label2.TabIndex = 0;
+            label2.Text = "ACTIONS";
+            // 
+            // btnCancel
+            // 
+            btnCancel.Location = new Point(204, 160);
+            btnCancel.Name = "btnCancel";
+            btnCancel.Size = new Size(75, 23);
+            btnCancel.TabIndex = 7;
+            btnCancel.Text = "STOP";
+            btnCancel.UseVisualStyleBackColor = true;
+            btnCancel.Click += btnCancel_Click;
+            // 
+            // btnStart
+            // 
+            btnStart.Location = new Point(6, 160);
+            btnStart.Name = "btnStart";
+            btnStart.Size = new Size(75, 23);
+            btnStart.TabIndex = 6;
+            btnStart.Text = "START";
+            btnStart.UseVisualStyleBackColor = true;
+            btnStart.Click += btnStart_Click;
+            // 
+            // panel2
+            // 
+            panel2.Controls.Add(txtProgramName);
+            panel2.Controls.Add(cmbAction);
+            panel2.Controls.Add(panel3);
+            panel2.Controls.Add(btnStart);
+            panel2.Controls.Add(btnCancel);
+            panel2.Dock = DockStyle.Fill;
+            panel2.Location = new Point(240, 0);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(291, 225);
+            panel2.TabIndex = 1;
+            // 
+            // txtProgramName
+            // 
+            txtProgramName.Location = new Point(6, 110);
+            txtProgramName.Name = "txtProgramName";
+            txtProgramName.PlaceholderText = "program name exact";
+            txtProgramName.Size = new Size(273, 23);
+            txtProgramName.TabIndex = 8;
+            txtProgramName.Visible = false;
+            // 
+            // cmbAction
+            // 
+            cmbAction.AutoCompleteCustomSource.AddRange(new string[] { "SHUT DOWN", "RESTART", "SLEEP", "LOG OFF", "PROGRAM", "CUSTOM" });
+            cmbAction.FormattingEnabled = true;
+            cmbAction.Items.AddRange(new object[] { "SHUT DOWN", "RESTART", "SLEEP", "LOG OFF", "LOCK SYSTEM", "OPEN PROGRAM" });
+            cmbAction.Location = new Point(6, 71);
+            cmbAction.Name = "cmbAction";
+            cmbAction.Size = new Size(273, 23);
+            cmbAction.TabIndex = 5;
+            cmbAction.Text = "select action";
+            cmbAction.SelectedIndexChanged += cmbAction_SelectedIndexChanged;
+            // 
+            // panel3
+            // 
+            panel3.BackColor = Color.FromArgb(26, 26, 26);
+            panel3.Controls.Add(label1);
+            panel3.Dock = DockStyle.Top;
+            panel3.Location = new Point(0, 0);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(291, 30);
+            panel3.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom;
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.Aquamarine;
+            label1.Location = new Point(103, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(79, 21);
+            label1.TabIndex = 0;
+            label1.Text = "ACTIONS";
+            // 
+            // timer1
+            // 
+            timer1.Tick += timer1_Tick;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(31, 31, 31);
+            ClientSize = new Size(531, 225);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "Form1";
+            Text = "ACTION TIMER";
+            Load += Form1_Load;
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            panel4.ResumeLayout(false);
+            panel4.PerformLayout();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            panel3.ResumeLayout(false);
+            panel3.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Panel panel2;
+        private Panel panel3;
+        private Label label1;
+        private Panel panel4;
+        private Label label3;
+        private Label label2;
+        private Label lblTime;
+        private TextBox txtTime;
+        private ComboBox cmbAction;
+        private Label lblCountdown;
+        private Button btnCancel;
+        private Button btnStart;
+        private System.Windows.Forms.Timer timer1;
+        private Label label4;
+        private RadioButton rdoCountdown;
+        private RadioButton rdoClock;
+        private DateTimePicker dtpClockTime;
+        private TextBox txtProgramName;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,322 @@
+﻿namespace ACTION_TIMER
+
+{
+    using System;
+    using System.Diagnostics;
+    using System.Threading.Tasks;
+    using System.Windows.Forms;
+    using System.Runtime.InteropServices;
+
+    public partial class Form1 : Form
+    {
+        bool warningShown = false;
+        bool cancelPending = false;
+
+        int timeLeft = 0;
+        string selectedAction = "";
+        DateTime targetTime;
+
+        [DllImport("user32.dll")]
+        public static extern void LockWorkStation();
+
+        List<string> recentPrograms = new List<string>();
+        string programToOpen = "";
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void cmbAction_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string action = cmbAction.SelectedItem?.ToString();
+
+            // Show textbox only if "Open Program" is selected
+            txtProgramName.Visible = (action == "OPEN PROGRAM");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            //if (int.TryParse(txtTime.Text, out int minutes))
+            //{
+            //    timeLeft = minutes * 60; // convert to seconds
+            //    selectedAction = cmbAction.SelectedItem?.ToString();
+
+            //    if (string.IsNullOrEmpty(selectedAction))
+            //    {
+            //        MessageBox.Show("Please select an action.");
+            //        return;
+            //    }
+
+            //    timer1.Interval = 1000; // 1 second
+            //    timer1.Start();
+            //    lblCountdown.Text = $"Time left: {timeLeft} sec";
+            //    btnStart.Enabled = false;
+            //    txtTime.Enabled = false;
+            //    cmbAction.Enabled = false;
+            //}
+            //else
+            //{
+            //    MessageBox.Show("Enter a valid time in minutes.");
+            //}
+
+            selectedAction = cmbAction.SelectedItem?.ToString();
+
+            if (string.IsNullOrEmpty(selectedAction))
+            {
+                MessageBox.Show("Please select an action.");
+                return;
+            }
+
+            if (rdoCountdown.Checked)
+            {
+                if (int.TryParse(txtTime.Text, out int minutes))
+                {
+                    timeLeft = minutes * 60; // seconds
+                    timer1.Interval = 1000;
+                    timer1.Start();
+                    lblCountdown.Text = $"Time left: {timeLeft} sec";
+                }
+                else
+                {
+                    MessageBox.Show("Enter a valid number of minutes.");
+                    return;
+                }
+            }
+            else if (rdoClock.Checked)
+            {
+                targetTime = dtpClockTime.Value;
+                if (targetTime < DateTime.Now)
+                {
+                    // if time already passed today, schedule for tomorrow
+                    targetTime = targetTime.AddDays(1);
+                }
+                timer1.Interval = 1000;
+                timer1.Start();
+                lblCountdown.Text = $"Action scheduled for {targetTime.ToLongTimeString()}";
+            }
+            if (cmbAction.SelectedItem?.ToString() == "OPEN PROGRAM")
+            {
+                programToOpen = txtProgramName.Text.Trim();
+
+                if (string.IsNullOrEmpty(programToOpen))
+                {
+                    MessageBox.Show("Please type a program name to open.");
+                    return;
+                }
+
+                // Remember it for later (avoid duplicates)
+                if (!recentPrograms.Contains(programToOpen))
+                    recentPrograms.Add(programToOpen);
+            }
+
+
+            btnStart.Enabled = false;
+            txtTime.Enabled = false;
+            cmbAction.Enabled = false;
+            rdoCountdown.Enabled = false;
+            rdoClock.Enabled = false;
+            dtpClockTime.Enabled = false;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            ////timeLeft--;
+            ////lblCountdown.Text = $"Time left: {timeLeft} sec";
+
+            ////if (timeLeft <= 0)
+            ////{
+            ////    timer1.Stop();
+            ////    PerformAction();
+            ////}
+
+
+            if (rdoCountdown.Checked)
+            {
+                timeLeft--;
+                lblCountdown.Text = $"Time left: {timeLeft} sec";
+
+                if (timeLeft <= 0)
+                {
+                    timer1.Stop();
+                    PerformAction();
+                }
+            }
+            else if (rdoClock.Checked)
+            {
+                TimeSpan remaining = targetTime - DateTime.Now;
+                lblCountdown.Text = $"Time left: {remaining.Hours:D2}:{remaining.Minutes:D2}:{remaining.Seconds:D2}";
+
+                if (remaining.TotalSeconds <= 0)
+                {
+                    timer1.Stop();
+                    PerformAction();
+                }
+            }
+
+
+
+            //if (rdoCountdown.Checked)
+            //{
+            //    timeLeft--;
+            //    lblCountdown.Text = $"Time left: {timeLeft} sec";
+
+            //    // show 5-second warning (only once)
+            //    if (timeLeft == 5 && !warningShown)
+            //    {
+            //        warningShown = true;
+            //        ShowWarningPopup();
+            //    }
+
+            //    if (timeLeft <= 0)
+            //    {
+            //        timer1.Stop();
+            //        if (!cancelPending)
+            //            PerformAction();
+            //    }
+            //}
+            //else if (rdoClock.Checked)
+            //{
+            //    TimeSpan remaining = targetTime - DateTime.Now;
+            //    lblCountdown.Text = $"Time left: {remaining.Hours:D2}:{remaining.Minutes:D2}:{remaining.Seconds:D2}";
+
+            //    if (remaining.TotalSeconds <= 10 && !warningShown)
+            //    {
+            //        warningShown = true;
+            //        ShowWarningPopup();
+            //    }
+
+            //    if (remaining.TotalSeconds <= 0)
+            //    {
+            //        timer1.Stop();
+            //        if (!cancelPending)
+            //            PerformAction();
+            //    }
+        //}
+        }
+
+        //private async void ShowWarningPopup()
+        //{
+        //    // Use a separate task so it doesn’t freeze the countdown
+        //    await Task.Run(() =>
+        //    {
+        //        MessageBox.Show("⚠️ Action will execute in 10 seconds!\nClick Cancel if you wish to stop.\ndo you want to STOP",
+        //            "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        //    });
+        //}
+
+
+        private void PerformAction()
+        {
+            switch (selectedAction)
+            {
+                case "SHUT DOWN":
+                    Process.Start("shutdown", "/s /t 0");
+                    break;
+                case "RESTART":
+                    Process.Start("shutdown", "/r /t 0");
+                    break;
+                case "SLEEP":
+                    Application.SetSuspendState(PowerState.Suspend, true, true);
+                    break;
+                case "LOG OFF":
+                    Process.Start("shutdown", "/l");
+                    break;
+                case "LOCK SYSTEM":
+                    LockWorkStation();
+                    break;
+                case "OPEN PROGRAM":
+                    if (string.IsNullOrEmpty(programToOpen))
+                    {
+                        MessageBox.Show("No program specified.");
+                        break;
+                    }
+
+                    try
+                    {
+                        // Try to start directly
+                        Process.Start(programToOpen);
+                    }
+                    catch
+                    {
+                        try
+                        {
+                            // If not found by name, search via Windows Search (Shell execute)
+                            Process.Start("explorer.exe", $"shell:appsFolder\\{programToOpen}");
+                        }
+                        catch
+                        {
+                            MessageBox.Show($"Could not open program: {programToOpen}");
+                        }
+                    }
+                    break;
+            }
+
+            MessageBox.Show("Action executed!");
+            btnStart.Enabled = true;
+            txtTime.Enabled = true;
+            cmbAction.Enabled = true;
+            warningShown = false;
+            cancelPending = false;
+
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            //timer1.Stop();
+            //lblCountdown.Text = "Timer cancelled.";
+            //btnStart.Enabled = true;
+            //txtTime.Enabled = true;
+            //cmbAction.Enabled = true;
+            //rdoCountdown.Enabled = true;
+            //rdoClock.Enabled = true;
+            //dtpClockTime.Enabled = true;
+
+            timer1.Stop();
+            cancelPending = true;
+            lblCountdown.Text = "Timer cancelled.";
+            btnStart.Enabled = true;
+            txtTime.Enabled = true;
+            cmbAction.Enabled = true;
+            rdoCountdown.Enabled = true;
+            rdoClock.Enabled = true;
+            dtpClockTime.Enabled = true;
+            warningShown = false;
+        }
+
+        private void rdoCountdown_CheckedChanged(object sender, EventArgs e)
+        {
+            txtTime.Enabled = rdoCountdown.Checked;
+            dtpClockTime.Enabled = !rdoCountdown.Checked;
+        }
+
+        private void rdoClock_CheckedChanged(object sender, EventArgs e)
+        {
+            txtTime.Enabled = !rdoClock.Checked;
+            dtpClockTime.Enabled = rdoClock.Checked;
+        }
+
+        private void dtpClockTime_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SaveRecentPrograms()
+        {
+            System.IO.File.WriteAllLines("recentPrograms.txt", recentPrograms);
+        }
+
+        private void LoadRecentPrograms()
+        {
+            if (System.IO.File.Exists("recentPrograms.txt"))
+                recentPrograms = System.IO.File.ReadAllLines("recentPrograms.txt").ToList();
+        }
+
+    }
+}
